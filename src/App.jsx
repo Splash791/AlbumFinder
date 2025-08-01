281bf0db03214b1d8654872e2a3c29f74fe64b35
Tyler Quach <tylerquach@Tylers-MacBook-Pro.local>
2025-08-01 08:15:12 -0700

    Added a search bar and when an artist name is inputted the Spotify API return artist ID

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,87 @@
 
 import './App.css'
+import {FormControl, InputGroup, Container, Button} from "react-bootstrap";
+import {useState, useEffect} from "react";
+
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+
+console.log('Client ID:', clientId);
+console.log('Client Secret:', clientSecret);
 
 function App() {
   
+  const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+
+  useEffect(() => {
+    let authParms = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        clientId +
+        "&client_secret=" +
+        clientSecret,
+      };
+
+      fetch("https://accounts.spotify.com/api/token", authParms)
+      .then((response) => response.json())
+      .then((data) => {
+        setAccessToken(data.access_token);
+      });
+  }, []);
+
+  async function search() {
+    let artistParamas = {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+    const artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      artistParamas
+    )
+    .then((result ) => result.json())
+    .then((data) => {
+        return data.artists.items[0].id;
+      });
+
+    console.log("Search Input: " + searchInput);
+    console.log("Artist ID: " + artistID);
+  }
 
   return (
-   <>
-      <h1>Codédex Project Template</h1>
-    </>
-  )
+    <Container>
+    <InputGroup>
+      <FormControl
+        placeholder="Search For Artist"
+        type="input"
+        aria-label="Search for an Artist"
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            search();
+          } // search function
+        }}
+        onChange={(event) => setSearchInput(event.target.value)} // setSearch
+        style={{
+          width: "300px",
+          height: "35px",
+          borderWidth: "0px",
+          borderStyle: "solid",
+          borderRadius: "5px",
+          marginRight: "10px",
+          paddingLeft: "10px",
+        }}
+      />
+      // search function
+      <Button onClick={search}>Search</Button>
+    </InputGroup>
+  </Container>
+  );
 }
 
 export default App
