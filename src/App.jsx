ee5164572c0be6a1b26083209f8cf318e81a29cb
Tyler Quach <tylerquach@Tylers-MacBook-Pro.local>
2025-08-01 08:52:07 -0700

    ability to display function cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import {FormControl, InputGroup, Container, Button} from "react-bootstrap";
+import {FormControl, InputGroup, Container, Button,Card,Row} from "react-bootstrap";
 import {useState, useEffect} from "react";
 
 
@@ -67,33 +67,102 @@ function App() {
   }
 
   return (
+    <>
+      <Container>
+      <InputGroup>
+        <FormControl
+          placeholder="Search For Artist"
+          type="input"
+          aria-label="Search for an Artist"
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              search();
+            } // search function
+          }}
+          onChange={(event) => setSearchInput(event.target.value)} // setSearch
+          style={{
+            width: "300px",
+            height: "35px",
+            borderWidth: "0px",
+            borderStyle: "solid",
+            borderRadius: "5px",
+            marginRight: "10px",
+            paddingLeft: "10px",
+          }}
+        />
+        // search function
+        <Button onClick={search}>Search</Button>
+      </InputGroup>
+    </Container>
+    
     <Container>
-    <InputGroup>
-      <FormControl
-        placeholder="Search For Artist"
-        type="input"
-        aria-label="Search for an Artist"
-        onKeyDown={(event) => {
-          if (event.key === "Enter") {
-            search();
-          } // search function
-        }}
-        onChange={(event) => setSearchInput(event.target.value)} // setSearch
-        style={{
-          width: "300px",
-          height: "35px",
-          borderWidth: "0px",
-          borderStyle: "solid",
-          borderRadius: "5px",
-          marginRight: "10px",
-          paddingLeft: "10px",
-        }}
-      />
-      // search function
-      <Button onClick={search}>Search</Button>
-    </InputGroup>
-  </Container>
-  );
+          <Row
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              justifyContent: "space-around",
+              alignContent: "center",
+            }}
+          >
+            {albums.map((album) => {
+              return (
+                <Card
+                  key={album.id}
+                  style={{
+                    backgroundColor: "white",
+                    margin: "10px",
+                    borderRadius: "5px",
+                    marginBottom: "30px",
+                  }}
+                >
+                  <Card.Img
+                    width={200}
+                    src={album.images[0].url}
+                    style={{
+                      borderRadius: "4%",
+                    }}
+                  />
+                  <Card.Body>
+                    <Card.Title
+                      style={{
+                        whiteSpace: "wrap",
+                        fontWeight: "bold",
+                        maxWidth: "200px",
+                        fontSize: "18px",
+                        marginTop: "10px",
+                        color: "black",
+                      }}
+                    >
+                      {album.name}
+                    </Card.Title>
+                    <Card.Text
+                      style={{
+                        color: "black",
+                      }}
+                    >
+                      Release Date: <br /> {album.release_date}
+                    </Card.Text>
+                    <Button
+                      href={album.external_urls.spotify}
+                      style={{
+                        backgroundColor: "black",
+                        color: "white",
+                        fontWeight: "bold",
+                        fontSize: "15px",
+                        borderRadius: "5px",
+                        padding: "10px",
+                      }}
+                    >
+                      Album Link
+                    </Button>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </Row>
+        </Container>
+      </>
+    );
 }
-
 export default App
